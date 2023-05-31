b41424d1346143f71fbd41fd8832375a99e0148a
wuyutao <wuyutaott@gmail.com>
2023-05-31 14:47:02 +0800

    init

---
+++ assets/src/HotUpdate.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('HotUpdate')
+export class HotUpdate extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
