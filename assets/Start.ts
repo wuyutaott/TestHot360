0e9be4b8f92853b3d049f0ca3c9928aadd57126c
wuyutao <wuyutaott@gmail.com>
2023-05-31 15:06:35 +0800

    打印可写路径

--- assets/Start.ts
+++ assets/Start.ts
@@ -1,10 +1,11 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, native, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Main')
 export class Main extends Component {
     start() {
-
+        let path = native.fileUtils.getWritablePath();
+        console.log('WritablePath = ', path);
     }
 
     update(deltaTime: number) {
